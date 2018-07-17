0a0ffb26e8d684c45b42a5f746a48a28be048885
helaine.jesus <helaine.jesus@itixti.com.br>
2018-07-17 04:41:58 -0300

    a

--- ApiCurso/Controllers/ValuesController.cs
+++ ApiCurso/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace ApiCurso.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value3" , "helaine" };
+            return new string[] { "value1", "value2", "value3" , "helaine", "Teste" };
         }
 
         // GET api/values/5
